473d58ae8f1e22232e7fbe491ac8da53631ca04b
Matthew Chambers <matthew@chambers.net.nz>
2014-04-28 22:53:36 +1200

    Add C# library which can hopefully let me use the razor view engine to generate html for non-whitespace sensitive pages relating to code navigation. Currently my plans are to generate html and store the result in blob storage as a mostly self-contained unit.

---
+++ HighlighterLib.Templating/Scripts/ViewModels.ts
@@ -0,0 +1,12 @@
+﻿/// <reference path="./typings/knockout/knockout.d.ts" />
+
+class FileIndexViewModel {
+    constructor (public test: number) {
+        var a = test * 5;
+    }
+    greet() {
+        var v = "asdf";
+        
+        return this.test;
+    }
+}
